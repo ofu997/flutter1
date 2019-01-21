cb414137ca4dcdc162a927610aefed51db0cff99
oliver fu <ofu997@gmail.com>
2019-01-21 15:39:09 -0800

    attempt navigation to product manager page

---
+++ lib/pages/product_manager_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ProductManagerPage extends StatefulWidget{
+  ProductManagerPage();
+
+  @override
+    State<StatefulWidget> createState() {
+      // TODO: implement createState
+      return _ProductManagerPageState();
+    }
+}
+
+class _ProductManagerPageState extends State<ProductManagerPage>{
+
+  @override
+    Widget build(BuildContext context) {
+      // TODO: implement build
+      return Column();
+    }
+}
--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import '../product_manager.dart';
-
+import './product_manager_page.dart';
 class ProductsPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
@@ -8,7 +8,14 @@ class ProductsPage extends StatelessWidget{
     return Scaffold(
       drawer: Drawer(child: Column(children: <Widget>[
         AppBar(automaticallyImplyLeading: false, title: Text('Choose'),), //automaticallyImplyLeading: whether to assume AppBar actions
-        ListTile(title: Text('Manage Products'), onTap: (){},)
+        ListTile(title: Text('Manage Products'), onTap: (){
+              Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                  builder: (BuildContext context) => ProductManagerPage()
+              ),
+            );
+        },)
       ],),),
       appBar: AppBar(
         title: Text('Food List'),
