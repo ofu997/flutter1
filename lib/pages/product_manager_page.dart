f6b275cbe8c769708c90f55b6814870c35c03964
oliver fu <ofu997@gmail.com>
2019-01-21 22:37:56 -0800

    add navigation back

--- lib/pages/product_manager_page.dart
+++ lib/pages/product_manager_page.dart
@@ -1,20 +1,46 @@
 import 'package:flutter/material.dart';
+import './products.dart';
 
-class ProductManagerPage extends StatefulWidget{
+class ProductManagerPage extends StatefulWidget {
   ProductManagerPage();
 
   @override
-    State<StatefulWidget> createState() {
-      // TODO: implement createState
-      return _ProductManagerPageState();
-    }
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _ProductManagerPageState();
+  }
 }
 
-class _ProductManagerPageState extends State<ProductManagerPage>{
-
+class _ProductManagerPageState extends State<ProductManagerPage> {
   @override
-    Widget build(BuildContext context) {
-      // TODO: implement build
-      return Column();
-    }
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            AppBar(// what shows up when hamburger menu is clicked
+              automaticallyImplyLeading: false,
+              title: Text('What is this?'),
+            ), //automaticallyImplyLeading: whether to assume AppBar actions
+            ListTile(
+              title: Text('all Products'),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (BuildContext context) => ProductsPage()));
+              },
+            )
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        title: Text('The product manager'),
+      ),
+      body: Center(
+        child: Text('Here goes all product'),
+      ),
+    );
+  }
 }
--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -6,17 +6,25 @@ class ProductsPage extends StatelessWidget{
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      drawer: Drawer(child: Column(children: <Widget>[
-        AppBar(automaticallyImplyLeading: false, title: Text('Choose'),), //automaticallyImplyLeading: whether to assume AppBar actions
-        ListTile(title: Text('Manage Products'), onTap: (){
-              Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(
-                  builder: (BuildContext context) => ProductManagerPage()
-              ),
-            );
-        },)
-      ],),),
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            AppBar(
+              automaticallyImplyLeading: false, 
+              title: Text('Choose'),
+              ), //automaticallyImplyLeading: whether to assume AppBar actions
+              ListTile(
+                  title: Text('Manage Products'),
+                  onTap: (){
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (BuildContext context) => ProductManagerPage()));
+                  },
+                )
+              ],
+            ),
+      ),
       appBar: AppBar(
         title: Text('Food List'),
       ),
