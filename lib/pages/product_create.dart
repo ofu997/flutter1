15377e3edebcd867ac15b9d6089cc73a3f791f23
oliver fu <ofu997@gmail.com>
2019-01-22 16:46:55 -0800

    + modal

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget{
+class ProductCreatePage extends StatelessWidget {
   @override
-    Widget build(BuildContext context) {
-      // TODO: implement build
-      return Center(child: Text('Create a product'),);
-    }
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: RaisedButton(
+        child: Text('Save'),
+        onPressed: (){
+          showModalBottomSheet(
+            context: context, builder: (BuildContext context){
+              return Center(child: Text('This is our modal'),);
+            }
+          );
+        },
+      ),
+    );
+  }
 }
