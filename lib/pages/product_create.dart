54dd11deec4e8d2e2735235d126eaf5fbb532621
oliver fu <ofu997@gmail.com>
2019-01-31 10:39:39 -0800

    + validation on product creating page

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -15,51 +15,70 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
   String _titleValue = '';
   String _descriptionValue = '';
   double _priceValue = 0.00;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   Widget _buildTitleTextField(){
-    return TextField(
-            decoration: InputDecoration(labelText: 'Product Title'),
-            onChanged: (String value) {
-              setState(() {
-                _titleValue = value;
-              });
-            },
-          );
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Product Title'),
+      validator: (String value){
+        if (value.isEmpty || value.length < 5){
+          return ('Title is required and should be 5+ characters long.');
+        }
+      },
+      onSaved: (String value){
+        setState(() {
+          _titleValue = value;
+        });
+      },
+    );
   }
 
   Widget _buildDescriptionTextField(){
-    return  TextField(
-            decoration: InputDecoration(labelText: 'Product Description'),
-            maxLines: 4,
-            onChanged: (String value) {
-              setState(() {
-                _descriptionValue = value;
-              });
-            },
-          );
+    return  TextFormField(
+      decoration: InputDecoration(labelText: 'Product Description'),
+      maxLines: 4,
+      validator: (String value){
+        if (value.isEmpty || value.length < 10){
+          return ('Description is required and should be 10+ characters long.');
+        }
+      },      
+            onSaved: (String value){
+        setState(() {
+          _descriptionValue = value;
+        });
+      }
+    );
   }
 
-Widget _buildPriceTextField(){
-  return  TextField(              
-            decoration: InputDecoration(labelText: 'Product Price'),          
-            keyboardType: TextInputType.number,
-            onChanged: (String value) {
-              setState(() {
-                _priceValue = double.parse(value);
-                print(_priceValue.toString());
-              });
-            },
-          );
-}
+  Widget _buildPriceTextField(){
+    return  TextFormField(              
+      decoration: InputDecoration(labelText: 'Product Price'),          
+      keyboardType: TextInputType.number,
+      validator: (String value){
+        if (value.isEmpty || !RegExp(r'^(?:[1-9]\d*|0)?(?:\.\d+)?$').hasMatch(value)){
+          return ('Price is required and should be a number.');
+        }
+      },      
+      onSaved: (String value){
+        setState(() {
+          _priceValue = double.parse(value);
+        });
+      }
+    );
+  }
 
-void _submitForm(){
-              final Map<String, dynamic> product = {
-                'title': _titleValue, 'description': _descriptionValue,
-              'price': _priceValue, 'image': 'assets/food.jpg'
-              };
-            widget.addProduct(product);//widget calls parent class ProductCreatePage
-            Navigator.pushReplacementNamed(context,'/products');// this method gives you no option of going back
-            }
+  void _submitForm(){
+    if(!_formKey.currentState.validate()){
+      return;
+    }
+    _formKey.currentState.save();
+    final Map<String, dynamic> product = {
+      'title': _titleValue, 'description': _descriptionValue,
+    'price': _priceValue, 'image': 'assets/food.jpg'
+    };
+    widget.addProduct(product);//widget calls parent class ProductCreatePage
+    Navigator.pushReplacementNamed(context,'/products');// this method gives you no option of going back
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -69,28 +88,31 @@ void _submitForm(){
     return Container(
       width: targetWidth,
       margin: EdgeInsets.all(10.0),
-      child: ListView(
-        padding: EdgeInsets.symmetric(horizontal: targetPadding /2),
-        children: <Widget>[//ListView children always take full available width
-          _buildTitleTextField(),
-          _buildDescriptionTextField(),
-          _buildPriceTextField(),
-          SizedBox(height: 25.0,),
-          // RaisedButton(
-          //   color: Theme.of(context).primaryColorLight,
-          //   child: Text('Save'),
-          //   onPressed: _submitForm,
-          // ),
-          GestureDetector(//offers other types of listener events
-            onTap: _submitForm,
-            child: Container(
-              color: Colors.green,
-              padding: EdgeInsets.all(5.0),
-              child: Text('My Button'),
+      child: Form(
+        key: _formKey,
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: targetPadding /2),
+          children: <Widget>[//ListView children always take full available width
+            _buildTitleTextField(),
+            _buildDescriptionTextField(),
+            _buildPriceTextField(),
+            SizedBox(height: 25.0,),
+            // RaisedButton(
+            //   color: Theme.of(context).primaryColorLight,
+            //   child: Text('Save'),
+            //   onPressed: _submitForm,
+            // ),
+            GestureDetector(//offers other types of listener events
+              onTap: _submitForm,
+              child: Container(
+                color: Theme.of(context).primaryColorLight,//Colors.orangeAccent,
+                padding: EdgeInsets.all(5.0),
+                child: Center(child: Text('My Button')),
+              ),
             ),
-          ),
-        ],
-      ),
+          ],
+        ),
+      )
     );
   }
 }
