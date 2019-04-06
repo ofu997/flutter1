25db7e6f0eae1da56d8ba5217d6de7824052db5e
ofu997 <ofu997@gmail.com>
2019-04-05 17:21:22 -0700

    maps: for location library, modify build.gradle, ios/Runner/info.plist, pubspec.yaml, and widgets/form_inputs/location

--- lib/widgets/form_inputs/location.dart
+++ lib/widgets/form_inputs/location.dart
@@ -1,23 +1,38 @@
+import 'dart:convert';
+import 'dart:async';
 import 'package:flutter/material.dart';
+
 import 'package:map_view/map_view.dart';
+import 'package:http/http.dart' as http;
+import 'package:location/location.dart' as geoloc;
 
 import '../helpers/ensure-visible.dart';
+import '../../models/location_data.dart';
+import '../../models/product.dart';
 
  class LocationInput extends StatefulWidget {
+  final Function setLocation;
+  final Product product;
+
+   LocationInput(this.setLocation, this.product);
   @override
   State<StatefulWidget> createState() {
     return _LocationInputState();
   }
 }
 
- class _LocationInputState extends State<LocationInput> {
+class _LocationInputState extends State<LocationInput> {
   Uri _staticMapUri;
+	LocationData _locationData;
   final FocusNode _addressInputFocusNode = FocusNode();
+  final TextEditingController _addressInputController = TextEditingController();
 
    @override
   void initState() {
     _addressInputFocusNode.addListener(_updateLocation);
-    getStaticMap();
+    if (widget.product != null){
+      _getStaticMap(widget.product.location.address, geocode: false);
+    }
     super.initState();
   }
 
@@ -27,23 +42,93 @@ import '../helpers/ensure-visible.dart';
     super.dispose();
   }
 
-   void getStaticMap() {
-    final StaticMapProvider staticMapViewProvider =
-        StaticMapProvider('AIzaSyC3nhgfXt7zasHjaV1j33_sW1QdInAIIPM');// Maps Static API
-    final Uri staticMapUri = staticMapViewProvider.getStaticUriWithMarkers(
-        [Marker('position', 'Position', 45.58508, -122.76477)],
-        center: Location(45.58508, -122.76477),
+   void _getStaticMap(String address, {bool geocode = true, double lat, double lng}) async{
+     if (address.isEmpty){
+       setState((){
+         _staticMapUri = null;
+       });
+       widget.setLocation(null);
+       return;
+     }
+     if (geocode){
+       final Uri uri = Uri.https(
+         'maps.googleapis.com',
+         '/maps/api/geocode/json',
+         {'address': address, 
+         'key': 'AIzaSyC3nhgfXt7zasHjaV1j33_sW1QdInAIIPM'
+         },
+       );
+       final http.Response response = await http.get(uri);
+       final decodedResponse = json.decode(response.body);
+       final formattedAddress = decodedResponse['results'][0]['formatted_address'];
+       final coords = decodedResponse['results'][0]['geometry']['location'];
+       _locationData = LocationData(
+         address: formattedAddress,
+        latitude: coords['lat'],
+        longitude: coords['lng']
+       );
+     }
+     else if (lat == null && lng == null){
+       _locationData = widget.product.location;
+     }
+     else {
+       _locationData = LocationData(address: address, latitude: lat, longitude: lng);
+     }
+     if (mounted) {
+      final StaticMapProvider staticMapViewProvider =
+      StaticMapProvider('AIzaSyC3nhgfXt7zasHjaV1j33_sW1QdInAIIPM');// Maps Static API
+      final Uri staticMapUri = staticMapViewProvider.getStaticUriWithMarkers(
+        [Marker('position', 'Position', _locationData.latitude, _locationData.longitude)],
+        center: Location(_locationData.latitude, _locationData.longitude),
         width: 500,
         height: 300,
-        maptype: StaticMapViewType.hybrid);
-    setState(() {
-      _staticMapUri = staticMapUri;
-    });
+        maptype: StaticMapViewType.roadmap
+      );
+        widget.setLocation(_locationData);
+
+      setState(() {
+        _addressInputController.text = _locationData.address;
+        _staticMapUri = staticMapUri;
+      });
+    }        
   }
 
-   void _updateLocation() {}
 
-   @override
+  Future<String> _getAddress(double lat, double lng) async {
+    final uri = Uri.https(
+      'maps.googleapis.com',
+      '/maps/api/geocode/json',
+      {
+        'latlng': '${lat.toString()},${lng.toString()}',
+        'key': 'AIzaSyC3nhgfXt7zasHjaV1j33_sW1QdInAIIPM'
+      },
+    );
+    final http.Response response = await http.get(uri);
+    final decodedResponse = json.decode(response.body);
+    final formattedAddress = decodedResponse['results'][0]['formatted_address'];
+    return formattedAddress;
+  }
+
+  void _getUserLocation() async {
+  final location = geoloc.Location();
+  final currentLocation = await location.getLocation();
+  final address = await _getAddress(
+      currentLocation.latitude, currentLocation.longitude);
+    _getStaticMap(
+      address,
+      geocode: false,
+      lat: currentLocation.latitude,
+      lng: currentLocation.longitude
+    );
+  }
+
+  void _updateLocation() {
+    if (!_addressInputFocusNode.hasFocus) {
+      _getStaticMap(_addressInputController.text);
+    }    
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
@@ -51,13 +136,43 @@ import '../helpers/ensure-visible.dart';
           focusNode: _addressInputFocusNode,
           child: TextFormField(
             focusNode: _addressInputFocusNode,
+            controller: _addressInputController,
+            validator: (String value){
+              if (_locationData == null || value.isEmpty){
+                return 'No valid location found.';
+              }
+            },
           ),
         ),
         SizedBox(
           height: 10.0,
         ),
-        Image.network(_staticMapUri.toString())
+        FlatButton(
+          child: Text('locate user'),
+          onPressed: _getUserLocation,
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        staticMapUri == null
+          ? Container()
+          : Image.network(_staticMapUri.toString())
       ],
     );
   }
+
+
+
+
+    //   final StaticMapProvider staticMapViewProvider =
+    //     StaticMapProvider('AIzaSyC3nhgfXt7zasHjaV1j33_sW1QdInAIIPM');// Maps Static API
+    // final Uri staticMapUri = staticMapViewProvider.getStaticUriWithMarkers(
+    //     [Marker('position', 'Position', 45.58508, -122.76477)],
+    //     center: Location(45.58508, -122.76477),
+    //     width: 500,
+    //     height: 300,
+    //     maptype: StaticMapViewType.hybrid);
+    // setState(() {
+    //   _staticMapUri = staticMapUri;
+    // });
 }
